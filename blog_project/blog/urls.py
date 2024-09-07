ec5ab7775f11094099665a3023fb503801d6a6d8
MalikBilalX <malik.bilal@xgrid.co>
2024-09-07 18:42:35 -0500

    update and add functionality

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BlogsListView, BlogDetailView
+from .views import BlogsListView, BlogDetailView, BlogCreateView, BlogUpdateView
 
 urlpatterns = [
     path('', BlogsListView.as_view(), name='home'),
-    path('blog/<int:pk>/', BlogDetailView.as_view(), name="blog_detail")
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/new/', BlogCreateView.as_view(), name='blog_new'),
+    path('blog/<int:pk>/edit/', BlogUpdateView.as_view(), name='blog_edit') 
 ]
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 
 # Create your views here.
@@ -12,3 +12,13 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog_detail.html'
     context_object_name = 'blog_detail'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'blog_new.html'
+    fields = ['title', 'author', 'body']
+    
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'blog_edit.html'
+    fields = ['title', 'body']
