8b4ef93221cb0b63ab5d2f015eef40dd5a359a9f
MalikBilalX <malik.bilal@xgrid.co>
2024-09-08 01:09:54 -0500

    signup functionality added

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
     path('blog/new/', BlogCreateView.as_view(), name='blog_new'),
     path('blog/<int:pk>/edit/', BlogUpdateView.as_view(), name='blog_edit'),
-    path('blog/<int:pk>/delete', BlogDeleteView.as_view(), name='blog_delete'),
+    path('blog/<int:pk>/delete/', BlogDeleteView.as_view(), name='blog_delete'),
 ]
